81de33797374a60e31f1a83275dc2309f6baf9a2
Ritu <singhritukumari192004@gmail.com>
2025-03-01 04:30:07 +0530

    Added expense screen and income screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,5 +102,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
 }
